f9c911da59485e8d14b8e21ebb8a17f82626b93b
dpidcoe <dpidcoe@gmail.com>
2024-04-14 11:00:17 -0700

    moved strategem definitions to config files

---
+++ config.h
@@ -0,0 +1,24 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "strategems.h"
+
+//put strategems to assign to keys here
+#define KEY0 AIRSTRIKE
+#define KEY1 ROCKET_STRIKE
+#define KEY2 SHIELD_PACK
+#define KEY3 QUASAR_CANNON
+#define KEY4 ""
+#define KEY5 ""
+#define KEY6 ""
+#define KEY7 ""
+#define KEY8 ""
+#define KEY9 ""
+#define KEY10 ""
+#define KEY11 ""
+#define KEY12 ""
+#define KEY13 ""
+#define KEY14 ""
+#define KEY15 ""
+
+#endif
